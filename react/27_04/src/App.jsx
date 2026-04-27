ffe774db4555be5a2071831b18fa3d47d1ffa7ee
yahalom98 <twixy100@gmail.com>
2026-04-27 19:39:30 +0300

    sdfsf

--- react/27_04/src/App.jsx
+++ react/27_04/src/App.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
 import Products from "./components/Products/Products";
 
+import axios from 'axios';
+import Nav from "./components/Nav/Nav";
+
 export default function App() {
   let [productsApi, setProductApi] = useState([]);
 
-  async function getProducts() {
-    let response = await fetch("https://fakestoreapi.com/products");
-    let data = await response.json();
-    setProductApi(data);
-    console.log(data);
+  // async function getProducts() {
+  //   let response = await fetch("https://fakestoreapi.com/products");
+  //   let data = await response.json();
+  //   setProductApi(data);
+  //   console.log(data);
+  // }
+
+    async function getProducts() {
+    let response = await axios.get('https://fakestoreapi.com/products')
+    console.log(response.data);
+        setProductApi(response.data);
+
   }
+
 
   useEffect(() => {
     getProducts();
@@ -17,7 +28,13 @@ export default function App() {
 
   return (
     <div>
+
+      <Nav/>
+
+
+
       {productsApi.map(function (value, id) {
+        console.log(value);
         return (
           <Products key={id}
             title={value.title}
